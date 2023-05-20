e1e97f41b6adf2a014e2df464c96e8bf49478a2d
Camran1234 <Camran750@gmail.com>
2023-05-19 18:29:32 -0600

    [FIX]

--- ptec_frontend/src/App.js
+++ ptec_frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
+import './styles/App.css';
+import './styles/Admin.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Register from './pages/Register'
+import Login from './pages/Login';
+import MenuAdmin from './pages/admin/MenuAdmin'
+import EncuestaCrud from './pages/admin/EncuestaCRUD'
+import Encuesta from './pages/admin/Encuesta'
+import VisualizadorResultados from './pages/admin/VisualizadorResultados'
+import Dashboard from './pages/admin/Dashboard'
+import MenuUsuario from './pages/user/MenuUsuario'
+import EncuestaConstructor from './pages/admin/EncuestaConstructor'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/admin" element={<MenuAdmin />} />
+          <Route path="/encuestasCrud" element={<EncuestaCrud />} />
+          <Route path="/constructor" element={<EncuestaConstructor />} />
+          <Route path="/encuesta/:idEncuesta" element={<Encuesta />} />
+          <Route path="/resultados" element={<VisualizadorResultados />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/usuario" element={<MenuUsuario />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
